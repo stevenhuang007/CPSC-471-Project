83b62e39f302539ea8a9540a7bad3ad47d04d39a
N-Mak1026 <104821035+N-Mak1026@users.noreply.github.com>
2024-11-26 16:07:18 -0700

    added drop downs
    added the drop downs for searching up specific player stats, will need to add more later but at least Ive understood a bit of the codeing

--- my-react-vite-app/src/pages/playerstats.tsx
+++ my-react-vite-app/src/pages/playerstats.tsx
@@ -13,6 +13,45 @@ function PlayerStats() {
           Player stats data will be displayed here...
         </p>
       </div>
+      {/*Centered box*/}
+      <div className="flex justify-center mt-6">
+        <div className ="w-full max-w-xl">
+          <div className="grid grid-cols-1 gap-6">
+          {/* Casino select select*/}
+           <div className="flex item-center">
+              <label htmlFor="Select Casino" className="text-md mr-4 whitespace-nowrap w-1/3">Select Casino</label>
+              <select id="Select Casino" className="p-2 border border-gray-300 rounded-md w-2/3">
+                <option value=""> None...</option>
+                <option value="MGM Grand">MGM Grand</option>
+                <option value="Ceasers">Ceasers</option>
+                <option value="Vedara">Vedara</option>
+              </select>
+            </div>
+            {/* table games select*/}
+            <div className="flex items-center">
+              <label htmlFor = "Select Table Game" className ="text-md mr-4 whitespace-nowrap w-1/3">Select Table Game</label>
+              <select id="Select Casino" className="p-2 border border-gray-300 rounded-md w-2/3">
+                <option value=""> None...</option>
+                <option value="Roulette">Roulette</option>
+                <option value="Black Jack">Black Jack</option>
+                <option value="Poker">Poker</option>
+                <option value="Baccarat">Baccarat</option>
+              </select>
+            </div>
+            {/* Machine games select*/}
+            <div className="flex items-center">
+              <label htmlFor = "Select Machine Game" className ="text-md mr-4 whitespace-nowrap w-1/3">Select Machine Game</label>
+              <select id="Select Casino" className="p-2 border border-gray-300 rounded-md w-2/3">
+                <option value=""> None...</option>
+                <option value="Slotomania">Slotomania</option>
+                <option value="Roulette Machine">Roulette Machine</option>
+                <option value="Poker Machine">Poker Machine</option>
+                <option value="5 DRAGONS">5 DRAGONS</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
