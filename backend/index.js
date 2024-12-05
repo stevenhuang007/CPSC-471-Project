6b92975997e8ee1b4c5f81a01543d4d9c071be89
N-Mak1026 <104821035+N-Mak1026@users.noreply.github.com>
2024-12-05 15:02:36 -0700

    connecting the database to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,41 @@
 import express from "express";
-import mysql from "mysql";
+import mysql from "mysql2";
+import cors from "cors"
 
 const app = express();
+app.use(cors());
 
 const db = mysql.createConnection({
-  host: "local",
+  host: "localhost",
   user: "root",
   password: "Davesburgers13!",
+  database: "casino"
   //   database: "test", enter database name here
 });
 
 app.get("/", (req, res) => {
   res.json("hello this is the backend");
+});
+
+app.get("/player", (req, res) => {
+  const q = "SELECT * FROM player";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.error("Query Error:", err); // Log detailed error
+      return res.status(500).json({ error: "Database query failed", details: err });
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/player", (req,res)=>{
+  const q = "INSERT INTO player ('Player_ID','Phone','Birth_Date','Total_Winnings','Machine_Winnings') VALUES (?)"
+  const values = ["2","5874360788","20030721","10000.69","0.69",]
+
+  db.query(q, [values], (err,data)=>{
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.listen(8800, () => {
